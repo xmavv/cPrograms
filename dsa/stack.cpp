97a0fa9c51cf8dd515cedb023e2cbafca61010a0
xmavv <xxmatheooo@gmail.com>
2026-02-01 16:16:36 +0100

    dynamic stack

--- dsa/stack.cpp
+++ dsa/stack.cpp
@@ -1,31 +1,82 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
 using namespace std;
 //malloc vs new
 //malloc only allocates memory
 //new is allocating memory and creating related object, thats why in other languages we use new operator!
 
 template<typename T>
+class AllocationStrategy {
+  public:
+    virtual bool push(T*& arr, int& item_count, int& capacity, T item) = 0;
+};
+
+template<typename T>
+class StaticAllocationStrategy : public AllocationStrategy<T> {
+  public:
+    bool push(T*& arr, int& item_count, int& capacity, T item) {
+      if(item_count >= capacity) return false;
+
+      arr[item_count] = item;
+      item_count++;
+      return true;
+    };
+};
+
+template<typename T>
+class DynamicAllocationStrategy : public AllocationStrategy<T> {
+  public:
+    bool push(T*& arr, int& item_count, int& capacity, T item) {
+      //oczywiscie mozna by uzywac wskaznikow ale my jestesmy koty i piszemy sami
+      T* newArr = new T[item_count+1];
+
+      for(int i=0; i<item_count; i++) {
+        newArr[i] = arr[i];
+      }
+
+      newArr[item_count] = item;
+
+      delete[] arr;
+      arr = newArr;
+
+      item_count++;
+      return true;
+    };
+};
+
+template<typename T>
 class Stack {
   private:
     T* arr;
+    AllocationStrategy<T>* strategy;
 
   public:
     int capacity = 0;
     int item_count = 0;
 
     //if capacity == -1 then its dynamic
-    //Stack() {}
+    Stack() {
+      this->capacity = -1;
+      this->strategy = new DynamicAllocationStrategy<T>();
+    }
+
     Stack(int capacity) {
       this->capacity = capacity;
-      this->arr = new T[capacity];
+
+      if(capacity == -1) {
+        this->arr = new T[0];
+        this->strategy = new DynamicAllocationStrategy<T>();
+      } 
+      else {
+        this->arr = new T[capacity];
+        this->strategy = new StaticAllocationStrategy<T>();
+      }
     }
 
     bool push(T item) {
-      if(this->item_count >= this->capacity) return false;
-
-      this->arr[this->item_count] = item;
-      this->item_count++;
-      return true;
+      return strategy->push(this->arr, this->item_count, this->capacity, item);
     }
 
     T pop() {
@@ -43,30 +94,32 @@ class Stack {
 };
 
 int main() {
-  Stack<int> stack(5);
+  Stack<int> stack(-1);
+  int isEmpty = stack.is_empty();
+  cout<<"empty: "<<isEmpty<<"\n";
   int poped = stack.pop();
-  bool push1 = stack.push(2);
-  int poped2 = stack.pop();
-  bool push2 = stack.push(3);
-  bool push3 = stack.push(45);
-  bool push4 = stack.push(512);
-  bool push5 = stack.push(241);
-  bool push6 = stack.push(123);
-  bool isEmpty1 = stack.is_empty();
-  cout<<"isempty1"<<isEmpty1<<"\n";
-  bool push7 = stack.push(555);
-  bool push8 = stack.push(555);
-  bool push9 = stack.push(555);
-  int peeked1 = stack.peek();
-  int poped3 = stack.pop();
-  cout<<"peeked"<<peeked1<<"\n"<<"poped"<<poped3<<"\n";
-  int poped4 = stack.pop();
-  int poped5 = stack.pop();
-  stack.pop();
-  stack.pop();
-  bool isEmpty2 = stack.is_empty();
-  cout<<"isempty2"<<isEmpty2<<"\n";
+  cout<<"poped: "<<poped<<"\n";
 
-  cout<<poped<<"\n"<<poped2<<"\n"<<poped3<<"\n"<<poped4<<"\n"<<poped5<<"\n\n\n\n";
-  cout<<push1<<"\n"<<push2<<"\n"<<push3<<"\n"<<push4<<"\n"<<push5<<"\n"<<push6<<"\n"<<push7<<"\n"<<push8<<"\n"<<push9<<"\n";
+  bool pushed = stack.push(5);
+  cout<<"pushed: "<<pushed<<"\n";
+  bool pushed2 = stack.push(6);
+  cout<<"pushed2: "<<pushed2<<"\n";
+  bool pushed3 = stack.push(1);
+  cout<<"pushed3: "<<pushed3<<"\n";
+
+  int pop1 = stack.pop();
+  cout<<"pop1: "<<pop1<<"\n";
+  int pop2 = stack.pop();
+  cout<<"pop2: "<<pop2<<"\n";
+  int pop3 = stack.pop();
+  cout<<"pop3: "<<pop3<<"\n";
+
+
+  Stack<float> stackStatic(2);
+  stackStatic.push(55.5);
+  stackStatic.push(777.21);
+  float popek1 = stackStatic.pop();
+  float popek2 = stackStatic.pop();
+  cout<<"popek1: "<<popek1<<"\n";
+  cout<<"popek2: "<<popek2<<"\n";
 }
